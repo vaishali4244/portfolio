ceea865bf639f2f109e219d996bb4ba6fa605679
VAISHALI <vaishali120895@gmail.com>
2023-07-01 01:32:06 +0530

    minor changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,11 +12,11 @@ const About = () => {
                 <div className="self">
                 <img className="profile" src={require("../../assets/profile.png")} alt=" vaishali" />
                 <p>
-                    I am a Self-taught programmer with an aspiration to become a React Developer who holds a M.Tech degree in Electronics and Communication Engineering from NIT Kurukshetra. Creating and maintaining excellent websites and applications for the internet is what I dream of. I am passionate about improving my skills and learning new technologies.
+                    I am a Self-taught programmer with an aspiration to become a React Developer who holds an M.Tech degree in Electronics and Communication Engineering from NIT Kurukshetra. Creating and maintaining excellent websites and applications for the internet is what I dream of. I am passionate about improving my skills and learning new technologies.
                     I've created projects using React.js, JavaScript, Redux, CSS, HTML, Node.js, Bootstrap and much more!
                 </p>
                 </div>
-                <a className = "button1"  href="https://docs.google.com/document/d/1UBF6WRm-E6aa-Z_XNQZzFwWkguI0HYjQmKh0YbGgPlA/edit?usp=sharing" target="_blank" rel="noreferrer">View Resume</a>
+                <a className = "button1"  href="https://drive.google.com/file/d/1C8ukNCBkleHNWAzg4kO1IvMwOh53vraN/view?usp=sharing" target="_blank" rel="noreferrer">View Resume</a>
             </div>
         </div>
     )
